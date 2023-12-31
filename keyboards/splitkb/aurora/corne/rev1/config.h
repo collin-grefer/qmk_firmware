57e400f079fa43f6488a7a68a3c856880e0c54f4
collin-grefer <collin.grefer@outlook.de>
2023-12-31 01:06:33 +0100

    splitkb corne default 5x3

--- keyboards/splitkb/aurora/corne/rev1/config.h
+++ keyboards/splitkb/aurora/corne/rev1/config.h
@@ -29,7 +29,7 @@
 // Not yet available in `info.json`
 #ifdef RGB_MATRIX_ENABLE
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
-#    define RGB_MATRIX_LED_COUNT 54
-#    define RGB_MATRIX_SPLIT { 27, 27 }
+#    define RGB_MATRIX_LED_COUNT 48
+#    define RGB_MATRIX_SPLIT { 24, 24 }
 #    define SPLIT_TRANSPORT_MIRROR
 #endif
